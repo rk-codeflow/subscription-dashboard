ffc6c1c13ac95e3273dafeeab3fcdb74c5e061d1
Raj Kiran Chaudhary <42111832+rk-codeflow@users.noreply.github.com>
2025-10-11 13:31:24 +0545

    active users card

--- src/components/StatCard/CardGrid.tsx
+++ src/components/StatCard/CardGrid.tsx
@@ -1,10 +1,10 @@
 import { FiUserCheck } from "react-icons/fi";
 import { IoMdCheckmarkCircleOutline } from "react-icons/io";
 import { RxCrossCircled } from "react-icons/rx";
-import { FaRegStar } from "react-icons/fa6";
 import Card from "./Card";
 import styles from "./Card.module.scss";
 import userSubscriptions from "../../data/subscriptions.json";
+import usersData from "../../data/users.json";
 
 // get active subscriptions
 const activeSubscriptions = userSubscriptions.filter((sub) => {
@@ -18,7 +18,8 @@ const expiredSubscriptions = userSubscriptions.filter((sub) => {
   return date < new Date();
 }).length;
 
-// get most popular package
+// get active users
+const activeUsers = usersData.filter((user) => user.active === "1").length;
 
 const CardGrid = () => {
   return (
@@ -48,9 +49,9 @@ const CardGrid = () => {
       />
 
       <Card
-        title="Most popular package"
-        value={10}
-        icon={<FaRegStar />}
+        title="Active users"
+        value={activeUsers}
+        icon={<FiUserCheck />}
         color="#AF57DC"
         bgColor="#dbbdeaff"
       />
