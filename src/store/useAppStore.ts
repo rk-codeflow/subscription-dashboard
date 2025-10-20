77318e61e72e3ee95fb0378e4eeac33ac6ca3479
Raj Kiran Chaudhary <42111832+rk-codeflow@users.noreply.github.com>
2025-10-20 06:52:49 +0545

    working on removing props drilling via zustand wip

---
+++ src/store/useAppStore.ts
@@ -0,0 +1,41 @@
+import { create } from "zustand";
+import type { SubscriptionProps, User } from "../interface/interface";
+
+interface AppState {
+  users: User[];
+  subscriptions: SubscriptionProps[];
+  subLoading: boolean;
+  userLoading: boolean;
+  fetchUsers: () => Promise<void>;
+  fetchSubscriptions: () => Promise<void>;
+}
+
+export const useAppStore = create<AppState>((set) => ({
+  users: [],
+  subscriptions: [],
+  userLoading: true,
+  subLoading: true,
+  fetchUsers: async () => {
+    try {
+      const res = await fetch("/data/users.json");
+      const data = await res.json();
+      set({ users: data });
+    } catch (error) {
+      console.log("Error loading data", error);
+    } finally {
+      set({ userLoading: false });
+    }
+  },
+
+  fetchSubscriptions: async () => {
+    try {
+      const res = await fetch("/data/users.json");
+      const data = await res.json();
+      set({ subscriptions: data });
+    } catch (error) {
+      console.log("Error loading data", error);
+    } finally {
+      set({ subLoading: false });
+    }
+  },
+}));
