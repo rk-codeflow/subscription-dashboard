1bb1b0d4152f9a337487aad6607eb8d5d489809a
Raj Kiran Chaudhary <42111832+rk-codeflow@users.noreply.github.com>
2025-10-21 08:19:43 +0545

    app.tsx file reverted

--- src/App.tsx
+++ src/App.tsx
@@ -4,57 +4,46 @@ import CardGrid from "./components/StatCard/CardGrid";
 import type { SubscriptionProps, User } from "./interface/interface";
 import Chart from "./components/Chart/Chart";
 import UserTable from "./components/UserTable/UserTable";
-import { useAppStore } from "./store/useAppStore";
 
 function App() {
-  // const [subLoading, setSubLoading] = useState(true);
-  // const [subscription, setSubscription] = useState<SubscriptionProps[]>([]);
-  // const [userLoading, setUserLoading] = useState(true);
-  // const [users, setUsers] = useState<User[]>([]);
-
-  // Extract everything from store
-  const {
-    users,
-    subscriptions,
-    subLoading,
-    userLoading,
-    fetchUsers,
-    fetchSubscriptions,
-  } = useAppStore();
+  const [subLoading, setSubLoading] = useState(true);
+  const [subscription, setSubscription] = useState<SubscriptionProps[]>([]);
+  const [userLoading, setUserLoading] = useState(true);
+  const [users, setUsers] = useState<User[]>([]);
 
   // Fetch subscriptions
-  // const fetchSubscriptions = async () => {
-  //   try {
-  //     const res = await fetch("/data/subscriptions.json");
-  //     const data = await res.json();
-  //     setSubscription(data);
-  //   } catch (error) {
-  //     console.log("Error loading data", error);
-  //   } finally {
-  //     setSubLoading(false);
-  //   }
-  // };
+  const fetchSubscriptions = async () => {
+    try {
+      const res = await fetch("/data/subscriptions.json");
+      const data = await res.json();
+      setSubscription(data);
+    } catch (error) {
+      console.log("Error loading data", error);
+    } finally {
+      setSubLoading(false);
+    }
+  };
 
   useEffect(() => {
     fetchSubscriptions();
-  }, [fetchSubscriptions]);
+  }, []);
 
   // Fetch users
-  // const fetchUsers = async () => {
-  //   try {
-  //     const res = await fetch("/data/users.json");
-  //     const data = await res.json();
-  //     setUsers(data);
-  //   } catch (error) {
-  //     console.log("Error loading data", error);
-  //   } finally {
-  //     setUserLoading(false);
-  //   }
-  // };
+  const fetchUsers = async () => {
+    try {
+      const res = await fetch("/data/users.json");
+      const data = await res.json();
+      setUsers(data);
+    } catch (error) {
+      console.log("Error loading data", error);
+    } finally {
+      setUserLoading(false);
+    }
+  };
 
   useEffect(() => {
     fetchUsers();
-  }, [fetchUsers]);
+  }, []);
 
   const loading = subLoading || userLoading;
 
@@ -64,10 +53,10 @@ function App() {
       {loading ? (
         <div className="loading">Loading...</div>
       ) : (
-        <CardGrid subscriptions={subscriptions} users={users} />
+        <CardGrid subscriptions={subscription} users={users} />
       )}
-      <Chart subscriptions={subscriptions} users={users} />
-      {/* <UserTable subscriptions={subscriptions} users={users} /> */}
+      <Chart subscriptions={subscription} users={users} />
+      <UserTable subscriptions={subscription} users={users} />
     </>
   );
 }
